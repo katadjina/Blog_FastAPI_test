133f12f64d504deeadea84c3f52a6d33feec4846
Kasia <kata.drzazgowska@gmail.com>
2023-08-15 20:15:58 +0200

    comm

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 from fastapi import FastAPI
+from fastapi import status
 from models import BlogType
+from typing import Optional
 
 
 
 app = FastAPI()
 
-@app.get("/blog/all")
-def get_all_blogs():
-    return {'message' : 'all posts'}
+@app.get('/blog/all')
+#setting an optional value for a parameter
+def get_all_blogs(page, page_size: Optional[int] = None):
+    return {'message' : f'All {page_size} blogs on page {page}'}
 
 
-@app.get("/blog/type/{type}")
+@app.get('/blog/type/{type}')
 def blog_type(type: BlogType):
     return {'message' : f'Blog type: {type.value}'}
 
 
-@app.get("/blog/{id}")
-#received param has to be of type int
+
+#### Example of error handling
+@app.get('/blog/{id}')
 def get_blog(id: int):
-    return {'message' : f'Post with id {id}'}
+    if id > 10:
+        return {'error' : f'Blog with id {id} not found'}
+    else:
+      return {'message' : f'Post with id {id}'}
 
 
